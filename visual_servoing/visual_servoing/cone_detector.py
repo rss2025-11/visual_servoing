2216e715d28b95b503126a007c6ba732edd01187
schannap <123418940+schannap@users.noreply.github.com>
2025-03-17 22:10:05 -0400

    Update cone_detector.py Lab 4 Submission

--- visual_servoing/visual_servoing/cone_detector.py
+++ visual_servoing/visual_servoing/cone_detector.py
@@ -49,8 +49,29 @@ class ConeDetector(Node):
         #################################
 
         image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+        input_image = image
+        offset_mult = 1 if self.LineFollower else 0
+        lower_orange =  np.array([0, 0, 0])
+        upper_orange = np.array([255,255,255])
 
-        bounding_box = cd_color_segmentation(image)
+        if self.LineFollower:
+            cropped_image = image[152:258, :]
+            input_image = cropped_image
+            lower_orange =  np.array([10, 120, 115])
+            upper_orange = np.array([15,255,255])
+        else:
+            #The 8, x, 80, allows for darker oranges to be picked up but not some reds
+            # the x,200,x prevents lighter browns from being registered
+            #28 allows for bright oranges to be picked up but not yellows
+            lower_orange =  np.array([5, 200, 160])
+            upper_orange = np.array([15,255,255])
+
+
+        # if self.LineFollower:
+        #     lower_orange =  np.array([5, 200, 160])
+        #     upper_orange = np.array([15,255,255])
+
+        bounding_box = cd_color_segmentation(input_image, (lower_orange, upper_orange))
 
         lower_right_corner = bounding_box[1]
 
@@ -59,13 +80,15 @@ class ConeDetector(Node):
         lower_middle_point =( (lower_left_corner[0] + lower_right_corner[0])/2, lower_right_corner[1] )
 
         cone_location_px_msg = ConeLocationPixel()
-        cone_location_px_msg.u = float(lower_middle_point[0])
-        cone_location_px_msg.v = float(lower_middle_point[1])
+        cone_location_px_msg.u = float(lower_middle_point[0]) 
+        cone_location_px_msg.v = float(lower_middle_point[1] + offset_mult*130)
         
         self.cone_pub.publish(cone_location_px_msg)
 
-        # debug_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
-        # self.debug_pub.publish(debug_msg)
+        cv2.rectangle(input_image, bounding_box[0], bounding_box[1], (0,0,255), 3)
+        
+        debug_msg = self.bridge.cv2_to_imgmsg(input_image, "bgr8")
+        self.debug_pub.publish(debug_msg)
 
 def main(args=None):
     rclpy.init(args=args)
