92a54aab4c39b7286c4170d40e9bc6c3509dc379
schannap <123418940+schannap@users.noreply.github.com>
2025-03-17 22:11:49 -0400

    Update color_segmentation.py Lab 4 Submission

--- visual_servoing/visual_servoing/computer_vision/color_segmentation.py
+++ visual_servoing/visual_servoing/computer_vision/color_segmentation.py
@@ -22,7 +22,7 @@ def image_print(img):
 	cv.waitKey(0)
 	cv.destroyAllWindows()
 
-def cd_color_segmentation(img, template=None):
+def cd_color_segmentation(img, template):
 	"""
 	Implement the cone detection using color segmentation algorithm
 	Input:
@@ -37,38 +37,58 @@ def cd_color_segmentation(img, template=None):
 	min_x, max_x = 0,0
 	min_y, max_y = 0,0
 	hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-	#Best ranges and contour selected so far
-	#Contour chosen: contour[0]
-	#Kernel size: 7,7 ; square
-	#lower_orange =  np.array([8, 100, 100])#online answer:[10, 50, 70]
-	#upper_orange = np.array([24,255,255])#online answer:[24, 255, 255]
-
-	lower_orange =  np.array([8, 100, 100])
-	upper_orange = np.array([24,255,255])
 
 	#Find orange pixels
-	orange_mask = cv.inRange(hsv_img, lower_orange, upper_orange)
+	# lower_orange =  np.array([5, 200, 160])
+	# upper_orange = np.array([15,255,255])
+	# orange_mask = cv.inRange(hsv_img, lower_orange, upper_orange)
+
+	orange_mask = cv.inRange(hsv_img, template[0], template[1])
+
+	# image_print(orange_mask)
 
 	#Open img (Erode then dilate)
-	kernel = cv.getStructuringElement(cv.MORPH_RECT,(7,7))
+	kernel = cv.getStructuringElement(cv.MORPH_RECT,(5,5))
 	processed_mask = cv.morphologyEx(orange_mask, cv.MORPH_OPEN, kernel)
 	processed_mask = cv.morphologyEx(processed_mask, cv.MORPH_CLOSE, kernel)
 
+	# image_print(processed_mask)
+
 	#Counture and find bounding rect
 	contours, _ = cv.findContours(processed_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-	cnt = contours[0]
-	# cnt = max(contours, key=cv.contourArea)
 
-	x,y,w,h = cv.boundingRect(cnt)
-	bounding_box = ((x,y),(x + w, y + h))
+	if(len(contours) > 0):
+		# cnt = contours[0]
+		cnt = max(contours, key=cv.contourArea)
+
+		x,y,w,h = cv.boundingRect(cnt)
+		bounding_box = ((x,y),(x + w, y + h))
+
+	else: 
+		bounding_box = ((0,0),(0,0))
 	########## YOUR CODE ENDS HERE ###########
 
 	# Return bounding box
 	return bounding_box
 
 # def main():
-# 	orange =  np.uint8([[[0,165,255 ]]])
-# 	hsv_orange = cv.cvtColor(orange, cv.COLOR_BGR2HSV)
-# 	print(f"{hsv_orange}")
+# 	# orange =  np.uint8([[[0,165,255 ]]])
+# 	# hsv_orange = cv.cvtColor(orange, cv.COLOR_BGR2HSV)
+# 	# print(f"{hsv_orange}")
+# 	lower_orange = np.array([9, 100, 125])
+# 	upper_orange = np.array([11, 255, 255])
+# 	image = cv.imread("./test_line_follower/line_test9.png")
+# 	bbox = cd_color_segmentation(image, (lower_orange, upper_orange))
+
+# 	print(f"bbox: {bbox[0]}, {bbox[1]}")
+
+# 	cv.rectangle(image, bbox[0], bbox[1], (0,255,0), 3)
+
+# 	cv.imwrite("./test_line_follower/test_output.jpg", image)
+
+	# image_print(image)
+
+
+
 
 # main()
